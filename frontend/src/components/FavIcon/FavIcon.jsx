86af0daeecff367040db549176c45940db2a9541
Miguel Muñoz <miguelmjimenez98@gmail.com>
2025-06-10 23:07:31 +0200

    Fixed animation in favIcon

--- frontend/src/components/FavIcon/FavIcon.jsx
+++ frontend/src/components/FavIcon/FavIcon.jsx
@@ -1,36 +1,31 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import './FavIcon.css';
 import { taskService } from "../../services/api";
 
 const FavIcon = ({task}) => {
-    const [filled, setFilled] = useState(false);
-
-    //Establecemos el icono de favoritos al renderizar
-    useEffect(() => {
-        setFilled(task.fav);
-    }, [])
+    const [filled, setFilled] = useState(task.fav);            //Estado que maneja si el icono esta coloreado o no
+    const [justClicked, setJustClicked] = useState(false);  //Estado que maneja si el icono debe renderizar la animacion de recien clickado
 
     const handleClick = (event) => {
         event.stopPropagation();
         const newFilled = !filled;
         setFilled(newFilled);
-        console.log('Task que voy a convertir en favorita: ', task);
+        setJustClicked(true);
+
         const taskData = {
             ...task,
             fav: newFilled
         };
-        console.log('Task favorita: ', taskData);
 
-        taskService.update(taskData._id, taskData);
-        
+        taskService.update(taskData._id, taskData);   
     }
 
     return (
         <div className="fav-icon-wrapper" onClick={(e)=>handleClick(e)}>
             <span className={`star ${filled ? 'filled' : ''}`}>★</span>
             
-            {filled && 
+            {filled && justClicked &&
                 <div className="spark-container">
                     {[...Array(8)].map((_, i) => (
                     <span key={i} className={`spark spark-${i}`} />
