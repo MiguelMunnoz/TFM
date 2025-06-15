60e8f8df5bd986abe50541a6833713834eeef764
Miguel Muñoz <miguelmjimenez98@gmail.com>
2025-06-15 22:30:47 +0200

    Fixed delete image

--- frontend/src/components/ImageGallery/ImageGallery.jsx
+++ frontend/src/components/ImageGallery/ImageGallery.jsx
@@ -3,6 +3,7 @@ import { imageService } from '../../services/api';
 import { useEffect, useState } from 'react';
 
 import Image from '../Image/Image';
+import Modal from '../Modal/Modal';
 import { useDispatch } from 'react-redux';
 import { updateTask } from '../../slices/taskSlice';
 import { taskService } from '../../services/api';
@@ -10,6 +11,8 @@ import { taskService } from '../../services/api';
 const ImageGallery = ({ task }) => {
     const [imageUrls, setImageUrls] = useState([]);
     const [imageZoomed, setImageZoomed] = useState(null);
+    const [showConfirm, setShowConfirm] = useState(false); // Mostrar o no el mensaje
+    const [imageToDelete, setImageToDelete] = useState(null); // Índice de la imagen seleccionada
     const dispatch = useDispatch();
     const images = task.images || [];
 
@@ -47,28 +50,36 @@ const ImageGallery = ({ task }) => {
         setImageZoomed(imgSrc);
     }
 
-    const handleDelete = async (index) => {
-        
+    const handleDelete = (index) => {
+        setShowConfirm(true);
+        setImageToDelete(index);
+    }
+
+    const confirmDelete = async () => {
         try {
             const updatedImages = [...task.images];
-            const [deletedImage] = updatedImages.splice(index, 1);
+            const [deletedImage] = updatedImages.splice(imageToDelete, 1);
 
             const updatedTask = {
                 ...task,
                 images: updatedImages
             };
 
-
             dispatch(updateTask(updatedTask));
             const res = await taskService.update(task._id, updatedTask);
-
             dispatch(updateTask(res.data));
+
             await imageService.deleteImage(deletedImage);
+
+            
 
         } catch (error) {
             console.error('[ERROR] Error deleting image:', error);
+        } finally {
+            setShowConfirm(false);
+            setImageToDelete(null);
         }
-    }
+    };
 
     return (
         <>
@@ -84,7 +95,7 @@ const ImageGallery = ({ task }) => {
                                     className="gallery-image"
                                     onClick={ () => handleClick(imgSrc) }
                                 />
-                                <button className="image-delete-button" onClick={() => handleDelete(index)}>X</button>
+                                <button className="image-delete-button" onClick={() => handleDelete(index)}> X </button>
                             </div>
                         ))}
                     </div>
@@ -93,6 +104,11 @@ const ImageGallery = ({ task }) => {
                 )}
                 { imageZoomed && (
                     <Image src={imageZoomed} onClick={() => setImageZoomed(null)} />
+                )}
+
+                {showConfirm && (
+                    <Modal type="image" mode="delete" onClose={() => setShowConfirm(false)} onDeleteConfirm={confirmDelete}
+                    />
                 )}
                 
             </div>
