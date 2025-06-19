d16e67e60960f08855953c3ce8b798d746b1a77f
Miguel Muñoz <miguelmjimenez98@gmail.com>
2025-06-19 14:52:44 +0200

    Using vercel env variables

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://tfm-armw.onrender.com', /*import.meta.env.VITE_API_URL,*//*'https://tfm-armw.onrender.com'*/
+    baseURL: import.meta.env.VITE_API_URL,/*'https://tfm-armw.onrender.com'*/
     timeout: 10000,
     withCredentials: true,
 });
